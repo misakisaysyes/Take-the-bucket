7469830bea678124826fef8aa7f96748934c85d9
tanjia <misakisaysyes@gmail.com>
2021-05-27 19:06:49 +0800

    revise traffic lights

--- src/App.js
+++ src/App.js
@@ -2,11 +2,18 @@ import React from 'react'
 import TrafficLightsControllor from './traffic-lights'
 
 const App = () => {
-  return <TrafficLightsControllor lights={[
-      { color: 'red', duration: 20000, twinkleDuration: 5000 },
-      { color: 'green', duration: 20000, twinkleDuration: 5000 },
-      { color: 'yellow', duration: 10000 },
-  ]}/>
+  return (
+    <div>
+      <div>
+        每天一个提桶小技巧
+      </div>
+      <TrafficLightsControllor lights={[
+        { color: 'red', duration: 20000, twinkleDuration: 5000 },
+        { color: 'green', duration: 20000, twinkleDuration: 5000 },
+        { color: 'yellow', duration: 10000 },
+      ]}/>
+    </div>
+  )
 }
 
 export default App
--- src/traffic-lights.js
+++ src/traffic-lights.js
@@ -1,6 +1,17 @@
 import React, { useState, useEffect } from 'react'
 import './index.css'
 
+/** 1. 信号灯控制器
+用 React 实现一个信号灯（交通灯）控制器，要求：
+1. 默认情况下，
+  1.1. 红灯亮20秒，并且最后5秒闪烁
+  1.2. 绿灯亮20秒，并且最后5秒闪烁
+  1.3. 黄灯亮10秒
+  1.4. 次序为 红-绿-黄-红-绿-黄
+2. 灯的个数、颜色、持续时间、闪烁时间、灯光次序都可配置，如：
+   lights=[{color: '#fff', duration: 10000, twinkleDuration: 5000}, ... ]
+*/
+
 const wait = time => new Promise((resolve, reject) => {
     setTimeout(() => {
         resolve()
@@ -53,10 +64,12 @@ const TrafficLightsControllor = props => {
     }
 
     return (
-        <div className="traffic-lights">
-            {
-                lightsView.map((color, idx) => <div key={idx} className="light" style={{ backgroundColor: color }}></div>)
-            }
+        <div>
+            <div className="traffic-lights">
+                {
+                    lightsView.map((color, idx) => <div key={idx} className="light" style={{ backgroundColor: color }}></div>)
+                }
+            </div>
         </div>
     )
 }
